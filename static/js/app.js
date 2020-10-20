51d0d12ceb267d57b7e6d27dd0fa02118a3df434
K Ruhl <ruhlk@hotmail.com>
2020-10-20 08:52:53 -0500

    additional code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,15 @@
-// import data with d3 and json
-d3.json('/data/samples.json').then((data) => {
-    console.log(data);
+// import data with d3 and json and set dropdown menu to names array
+let data = []
+// read the json file in and set it to a variable. Then set that variable to the empty array "data"
+d3.json("samples.json").then((sample) => {
+    console.log(sample);
+    data = sample;
+
+    // 
+    data['names'].forEach(dropDownMenu => {
+        d3.select("#selDataset")
+        .append("option")
+        .text(dropDownMenu)
+        .property("value",dropDownMenu)
+    });
 });
